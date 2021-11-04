71098aa0ca1578c359471e323c4300bac9b659be
Collins Igeh <igehac@gmail.com>
2021-11-04 11:03:39 +0100

    implemented state for the App compenent to allow for dynamic user list display. And made neccesary adjustment in the UserList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewUserForm from "./modules/NewUser/NewUserForm";
 import UserList from "./modules/UserList/UserList";
 
 function App() {
+  const [users, setUsers] = useState([
+    { username: "Mac", age: 31 },
+    { username: "Collins", age: 34 },
+  ]);
+
+  const addUserHandler = (newUser) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, newUser];
+    });
+  };
+
   return (
     <div>
-      <NewUserForm />
-      <UserList />
+      <NewUserForm onAddUser={addUserHandler} />
+      {users.length > 0 && <UserList users={users} />}
     </div>
   );
 }
--- src/modules/UserList/UserList.js
+++ src/modules/UserList/UserList.js
@@ -3,12 +3,16 @@ import React from "react";
 import "../UI/UIStyles.css";
 import styles from "./UserList.module.css";
 
-const UserList = () => {
-
-    return <ul className="card">
-        <li className={styles.userDetails}>Mac (31 years old)</li>
-        <li className={styles.userDetails}>Collins (34 years old)</li>
-    </ul>;
-}
+const UserList = (props) => {
+  return (
+    <ul className="card">
+      {props.users.map((user) => (
+        <li className={styles.userDetails}>
+          {user.username} ({user.age} years old)
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default UserList;
